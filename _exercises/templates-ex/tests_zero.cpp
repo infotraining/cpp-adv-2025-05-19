70689e385d19b38a7c00d1295fa31f319b3b2764
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-05-20 16:09:50 +0200

    Refactor zero function implementation and update test cases for clarity

--- _exercises/templates-ex/tests_zero.cpp
+++ _exercises/templates-ex/tests_zero.cpp
@@ -7,29 +7,48 @@
 namespace Exercise
 {
     // TODO: Implement a function that sets all elements of a container to zero (default value).
+    namespace Ver_1
+    {
+        template <typename T>
+        void zero(T& container)
+        {
+            T newContainer(container.size());
+            container = newContainer;
+        }
+    } // namespace Ver_1
 
-    
+    template <typename Container>
+    void zero(Container& container)
+    {
+        using TValue = typename Container::value_type;
+
+        for (auto& item : container)
+        {
+            item = TValue{};
+        }
+    }
+
 } // namespace Exercise
 
 TEST_CASE("zero - Step 1")
 {
     using namespace Exercise;
 
-    // SECTION("vector<int>")
-    // {
-    //     std::vector<int> vec = {1, 2, 3, 4};
+    SECTION("vector<int>")
+    {
+        std::vector<int> vec = {1, 2, 3, 4};
 
-    //     zero(vec);
+        zero(vec);
 
-    //     REQUIRE(vec == std::vector{0, 0, 0, 0});
-    // }
+        REQUIRE(vec == std::vector{0, 0, 0, 0});
+    }
 
-    // SECTION("list<std::string>")
-    // {
-    //     std::list<std::string> lst = {"one", "two", "three"};
+    SECTION("list<std::string>")
+    {
+        std::list<std::string> lst = {"one", "two", "three"};
 
-    //     zero(lst);
+        zero(lst);
 
-    //     REQUIRE(lst == std::list<std::string>{"", "", ""});
-    // }
+        REQUIRE(lst == std::list<std::string>{"", "", ""});
+    }
 }
