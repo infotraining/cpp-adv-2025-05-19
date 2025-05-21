33d1a8645e1ce6101a1c09f0c3370face3970a4c
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-05-21 14:05:34 +0200

    Implement type traits for IsSame and add get_value function with tests for type comparison

--- templates/tests_templates.cpp
+++ templates/tests_templates.cpp
@@ -1,10 +1,12 @@
 #include "helpers.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <deque>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
+#include <map>
 
 using Helpers::Gadget;
 
@@ -54,7 +56,7 @@ namespace ReturnTypeFromTemplateFunction
         {
             return a < b ? a : b;
         }
-    }
+    } // namespace Ver_2
 
     namespace Ver_3
     {
@@ -65,7 +67,7 @@ namespace ReturnTypeFromTemplateFunction
                 return b;
             return a;
         }
-    }
+    } // namespace Ver_3
 
     namespace Ver_4
     {
@@ -74,7 +76,7 @@ namespace ReturnTypeFromTemplateFunction
         {
             return a < b ? a : b;
         }
-    }
+    } // namespace Ver_4
 } // namespace ReturnTypeFromTemplateFunction
 
 TEST_CASE("return types from template functions")
@@ -88,17 +90,268 @@ TEST_CASE("return types from template functions")
     REQUIRE(ReturnTypeFromTemplateFunction::Ver_4::maximum(a, b) == 42);
 }
 
+template <typename T, typename TContainer = std::vector<T>>
+class Stack
+{
+    TContainer items_;
+
+public:
+    using value_type = T;
+    using reference_type = T&;
+    using const_reference_type = const T&;
+
+    Stack() = default;
+
+    bool empty() const
+    {
+        return items_.empty();
+    }
+
+    // void push(const T& item)
+    // {
+    //     items_.push_back(item);
+    // }
+
+    // void push(T&& item)
+    // {
+    //     items_.push_back(std::move(item));
+    // }
+
+    template <typename TItem>
+    void push(TItem&& item)
+    {
+        items_.push_back(std::forward<TItem>(item));
+    }
+
+    size_t size() const;
+
+    const_reference_type top() const
+    {
+        return items_.back();
+    }
+
+    void pop()
+    {
+        items_.pop_back();
+    }
+};
+
+template <typename T, typename TContainer>
+size_t Stack<T, TContainer>::size() const
+{
+    return items_.size();
+}
+
 TEST_CASE("class templates")
 {
-    // TODO
+    Stack<int, std::deque<int>> stack_int;
+
+    stack_int.push(42);
+    stack_int.push(665);
+    stack_int.push(15);
+
+    REQUIRE(stack_int.top() == 15);
+    stack_int.pop();
+    REQUIRE(stack_int.size() == 2);
 }
+
+template <typename Container>
+void zero(Container& container)
+{
+    using TValue = typename Container::value_type;
+
+    for (auto& item : container)
+    {
+        item = TValue{};
+    }
+}
+
+template <typename T, size_t N>
+struct Array
+{
+    using iterator = T*;
+    using const_iterator = const T*;
+    using value_type = T;
+    using reference = T;
+    using const_reference = T&;
+
+    T items[N];
+
+    size_t size() const
+    {
+        return N;
+    }
+
+    reference operator[](size_t index)
+    {
+        return items[index];
+    }
+
+    const_reference operator[](size_t index) const
+    {
+        return items[index];
+    }
+
+    iterator begin()
+    {
+        return items;
+    }
+
+    const_iterator begin() const
+    {
+        return items;
+    }
+
+    iterator end()
+    {
+        return items + N;
+    }
+
+    const_iterator end() const
+    {
+        return items + N;
+    }
+};
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream& out, const Array<T, N>& arr)
+{
+    out << "[ ";
+    for (const auto& item : arr)
+        out << item << " ";
+    out << "]";
+    return out;
+}
+
+template <typename T>
+void print(const T& container)
+{
+    std::cout << "[ " << std::size(container) << "; ";
+    for (const auto& item : container)
+    {
+        std::cout << item << " ";
+    }
+    std::cout << "]\n";
+}
+
+TEST_CASE("NTTP - Non-Type Template Params")
+{
+    Array<int, 32> buffer;
+
+    zero(buffer);
+    print(buffer);
+
+    std::cout << "buffer: " << buffer << "\n";
+}
+
+namespace Explain
+{
+    template <typename TContainer>
+    size_t size(const TContainer& container)
+    {
+        return container.size();
+    }
+
+    template <typename T, size_t N>
+    size_t size(const T (&arr)[N])
+    {
+        return N;
+    }
+} // namespace Explain
+
+TEST_CASE("deducing size of array")
+{
+    std::vector<int> vec = {1, 2, 3};
+    print(vec);
+    REQUIRE(Explain::size(vec) == 3);
+
+    int tab[5] = {1, 2, 3, 4, 5};
+    REQUIRE(Explain::size(tab) == 5);
+    print(tab);
+}
+namespace TemplateTemplateParams
+{
+    template <typename T, 
+              template <typename, typename> class Container = std::vector, 
+              typename TAllocator = std::allocator<T>>
+    class Stack
+    {
+        Container<T, TAllocator> items_;
+
+    public:
+        using value_type = T;
+        using reference_type = T&;
+        using const_reference_type = const T&;
+
+        Stack() = default;
+
+        bool empty() const
+        {
+            return items_.empty();
+        }
+
+        void push(const T& item)
+        {
+            items_.push_back(item);
+        }
+
+        void push(T&& item)
+        {
+            items_.push_back(std::move(item));
+        }
+
+        size_t size() const;
+
+        const_reference_type top() const
+        {
+            return items_.back();
+        }
+
+        void pop()
+        {
+            items_.pop_back();
+        }
+    };
+
+    template <typename T, template <typename, typename> class Container, typename TAllocator>
+    size_t Stack<T, Container, TAllocator>::size() const
+    {
+        return items_.size();
+    }
+} // namespace TemplateTemplateParams
+
+TEST_CASE("class templates with template params")
+{
+    TemplateTemplateParams::Stack<int, std::deque> stack_int;
+
+    stack_int.push(42);
+    stack_int.push(665);
+    stack_int.push(15);
+
+    REQUIRE(stack_int.top() == 15);
+    stack_int.pop();
+    REQUIRE(stack_int.size() == 2);
+}
+
+template <typename TValue>
+using Dictionary = std::map<std::string, TValue>;
+
+typedef int Integer;
+using Real = double;
 
 TEST_CASE("template aliases")
 {
-    // TODO
+    Dictionary<int> dict = { {"one", 1}, {"two", 2} };
+    Dictionary<double> math_dict = { { "pi", 3.14 } };
 }
+
+template <typename T>
+const T pi_v = 3.1415;
 
 TEST_CASE("template variables")
 {
-    // TODO
+    float radius = 2.0;
+
+    auto area = pi_v<float> * pi_v<float> * radius;
+
 }
--- templates/tests_type_traits.cpp
+++ templates/tests_type_traits.cpp
@@ -1,6 +1,86 @@
 #include <catch2/catch_test_macros.hpp>
+#include <iostream>
+
+int value(int value)
+{
+    return value;
+}
+
+/////////////////////////////////////////////////
+
+template <int N>
+struct Value
+{
+    static constexpr int value = N;
+};
+
+template <int N>
+constexpr int Value_v = Value<N>::value; 
+
+/////////////////////////////////////////////////
+
+template <typename T>
+struct Identity
+{
+    using type = T;
+};
+
+template <typename T>
+using Identity_t = typename Identity<T>::type;
+
+/////////////////////////////////////////////////
+
+template <typename T1, typename T2>
+struct IsSame
+{
+    static constexpr bool value = false;
+};
+
+template <typename T>
+struct IsSame<T, T>
+{
+    static constexpr bool value = true;
+};
+
+template <typename T1, typename T2>
+constexpr bool IsSame_v = IsSame<T1, T2>::value;
 
 TEST_CASE("type traits")
 {   
-    // TODO   
+    REQUIRE(value(42) == 42);
+    REQUIRE(Value_v<42> == 42);
+
+    using T1 = int;
+    using T2 = Identity_t<int>;
+
+    if constexpr (std::is_same_v<T1, T2>)
+    {
+        std::cout << "types are the same\n";
+    }
+    else
+    {
+        std::cout << "types are the different\n";
+    }
+}
+
+///////////////////////////////////////////////////////
+
+// trait IsPointer_v
+
+template <typename T>
+auto get_value(T obj)
+{
+    // return obj;
+
+    // return *obj;
+}
+
+TEST_CASE("get_value")
+{
+    int x = 42;
+    REQUIRE(get_value(x) == 42);
+
+
+    int* ptr = &x;
+    REQUIRE(get_value(ptr) == 42);
 }
