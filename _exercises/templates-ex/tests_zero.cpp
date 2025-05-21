4f5d2bad8caded7e8330520aee87e3bd358983d3
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-05-21 10:54:48 +0200

    Refactor zero function implementation and enhance type deduction tests for clarity

--- _exercises/templates-ex/tests_zero.cpp
+++ _exercises/templates-ex/tests_zero.cpp
@@ -1,8 +1,13 @@
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
+
 #include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <list>
 #include <string>
 #include <vector>
+#include <iostream>
 
 namespace Exercise
 {
@@ -17,16 +22,19 @@ namespace Exercise
         }
     } // namespace Ver_1
 
-    template <typename Container>
-    void zero(Container& container)
+    inline namespace Ver_2
     {
-        using TValue = typename Container::value_type;
-
-        for (auto& item : container)
+        template <typename Container>
+        void zero(Container& container)
         {
-            item = TValue{};
+            using TValue = typename Container::value_type;
+
+            for (auto& item : container)
+            {
+                item = TValue{};
+            }
         }
-    }
+    } // namespace Ver_2
 
 } // namespace Exercise
 
@@ -50,5 +58,101 @@ TEST_CASE("zero - Step 1")
         zero(lst);
 
         REQUIRE(lst == std::list<std::string>{"", "", ""});
+    }
+}
+
+void foobar(int)
+{}
+
+template <typename T>
+void deduce_1(T arg)
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
+template <typename T>
+void deduce_2(T& arg)
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
+template <typename T>
+void deduce_3(T&& arg)
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
+namespace Cpp20
+{
+    void deduce_1(auto arg)
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+    }
+}
+
+TEST_CASE("type deduction")
+{
+    int x = 10;
+    const int cx = 42;
+    int& ref_x = x;
+    const int& cref_x = x;
+    int tab[10];   
+
+    SECTION("deduce - auto")
+    {
+        auto ax1 = x;      // int
+        deduce_1(x);         // deduce<int>(int arg)
+        Cpp20::deduce_1(x);  // deduce<int>(int arg)
+
+        auto ax2 = cx;     // int - const is removed
+        deduce_1(cx);      // deduce<int>(int)
+
+        auto ax3 = ref_x;  // int - ref is removed
+        deduce_1(ref_x);   // deduce<int>(int)
+
+        auto ax4 = cref_x; // int - const ref is removed
+        deduce_1(cref_x);  // deduce<int>(int)
+
+        auto ax5 = tab;    // int* - array decays into pointer
+        deduce_1(tab);     // deduce<int*>(int*)
+
+        auto ax6 = foobar; // void(*ax6)(int)
+        deduce_1(foobar);  // deduce<void(*)(int)>(void(*)(int))
+    }
+
+    SECTION("deduce - auto&")
+    {
+        auto& ax1 = x;      // int&
+        deduce_2(x);        // deduce_2<int>(int&)
+
+        auto& ax2 = cx;     // const int& - const is not removed
+        deduce_2(cx);       // deduce_2<const int>(const int&)
+
+        auto& ax3 = ref_x;  // int&
+        deduce_2(ref_x);    // deduce_2<int>(int&)
+
+        auto& ax4 = cref_x; // const int&
+        deduce_2(cref_x);   // deduce_2<const int>(const int&)
+
+        auto& ax5 = tab;    // int(&ax5)[10]
+        deduce_2(tab);      // deduce_2<int[10]>(int(&)[10])
+
+        auto& ax6 = foobar; // void(&ax6)(int)
+        deduce_2(foobar);   // deduce_2(void(int))(void(&)(int))
+    }
+
+    SECTION("deduce - auto&&")
+    {
+        SECTION("lvalue")
+        {
+            auto&& ax1 = x;  // int&
+            deduce_3(x);     // deduce_3<int&>(int&)
+        }
+
+        SECTION("rvalue")
+        {
+            auto&& ax2 = 42; // int&&
+            deduce_3(42);    // deduce_3<int>(int&&)
+        }
     }
 }
